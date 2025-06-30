ab08398e3f272be4cb511313aa825748e3316bc7
CharlyHilasaca <charly.hilasaca@tecsup.edu.pe>
2025-06-30 12:54:08 -0500

    aaaa

--- controllers/PagosController.js
+++ controllers/PagosController.js
@@ -119,9 +119,9 @@ exports.pagarConCheckoutPro = async (req, res) => {
       }
     ],
     back_urls: {
-      success: "https://proyectfront.onrender.com/",
-      failure: "https://proyectfront.onrender.com/",
-      pending: "https://proyectfront.onrender.com/"
+      success: "https://proyectfront.onrender.com/pago-exitoso",
+      failure: "https://proyectfront.onrender.com/pago-fallido",
+      pending: "https://proyectfront.onrender.com/pago-pendiente"
     },
     sandbox_mode: true
   };
